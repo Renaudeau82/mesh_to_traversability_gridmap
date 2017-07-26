776a235b0beb4a4539597fb2d813c824f15a7391
Brice Renaudeau <brice_ren@hotmail.fr>
2017-07-26 15:17:48 +0200

    update normals

--- src/library/mesh_to_traversability_gridmap.cpp
+++ src/library/mesh_to_traversability_gridmap.cpp
@@ -100,7 +100,7 @@ void MeshToGridMapConverter::meshCallback(const pcl_msgs::PolygonMesh& mesh_msg)
     }
 
     /// pre treatment of grid_map
-    if(verbose_) ROS_INFO_STREAM("Treatment of grid_map");
+    if(verbose_) ROS_INFO_STREAM("Treatment of grid_map                      ");
     time1 = ros::Time::now();
     map.add("normal_x", 0.0);
     map.add("normal_y", 0.0);
@@ -117,59 +117,64 @@ void MeshToGridMapConverter::meshCallback(const pcl_msgs::PolygonMesh& mesh_msg)
         if(map.at("elevation", *it) > zmax)
             zmax = map.at("elevation", *it);
     }
-    double scaleZ = (zmax + 0.65)/255;
+    double scaleZ = (zmax + 0.65)/255; //225 if using CV_8U in elevation toIamge
 
     /// Use Opencv to compute normals
     cv::Mat mapImage;
-    grid_map::GridMapCvConverter::toImage<unsigned short, 1>(map, "elevation", CV_16UC1, -0.60, zmax, mapImage);
+    //grid_map::GridMapCvConverter::toImage<unsigned short, 1>(map, "elevation", CV_16UC1, -0.65, zmax, mapImage);
+    grid_map::GridMapCvConverter::toImage<unsigned char, 1>(map, "elevation", CV_8UC1, -0.65, zmax, mapImage);
     cv::medianBlur(mapImage,mapImage,3);
 
     // gradiant
     int scale = 1;  int delta = 0;  int ddepth = CV_32F;
     cv::Mat grad_x, grad_y;
-    //cv::Sobel( mapImage, grad_x, ddepth, 1, 0, 3, scale, delta, cv::BORDER_DEFAULT );
-    //cv::Sobel( mapImage, grad_y, ddepth, 0, 1, 3, scale, delta, cv::BORDER_DEFAULT );
     cv::Scharr( mapImage, grad_x, ddepth, 1, 0, scale, delta, cv::BORDER_DEFAULT );
     cv::Scharr( mapImage, grad_y, ddepth, 0, 1, scale, delta, cv::BORDER_DEFAULT );
     cv::Mat gradiant = cv::Mat::zeros(mapImage.rows, mapImage.cols, CV_32F);
     cv::magnitude(grad_x,grad_y,gradiant);
-    cv::normalize(gradiant, gradiant,0.0, 255.0, cv::NORM_MINMAX, CV_32F);
-    //cv::normalize(gradiant, gradiant,0x00, 0xFF, cv::NORM_MINMAX, CV_8U);
     // smoothing of the gradiant
-    cv::GaussianBlur(gradiant,gradiant,cv::Size(5,5),0,0);
+    cv::GaussianBlur(gradiant,gradiant,cv::Size(3,3),0,0);
 
     // orientation
     cv::Mat orientation = cv::Mat::zeros(mapImage.rows, mapImage.cols, CV_32F);
-    grad_y.convertTo(grad_y,CV_32F);
-    grad_x.convertTo(grad_x,CV_32F);
     cv::phase(grad_x, grad_y, orientation,true);
     cv::normalize(orientation, orientation, 0x00, 0xFF, cv::NORM_MINMAX, CV_8U);
-    // create new layers to store the data
-    grid_map::GridMapCvConverter::addLayerFromImage<float, 1>(gradiant, "gradiant", map, 0.0, 255.0);
     grid_map::GridMapCvConverter::addLayerFromImage<unsigned char, 1>(orientation, "orientation", map, 0, 255);
 
     //Normals
-    for (grid_map::GridMapIterator it(map); !it.isPastEnd(); ++it)
+    cv::Mat normal_x = cv::Mat::zeros(mapImage.rows, mapImage.cols, CV_32F);
+    cv::Mat normal_y = cv::Mat::zeros(mapImage.rows, mapImage.cols, CV_32F);
+    cv::Mat normal_z = cv::Mat::zeros(mapImage.rows, mapImage.cols, CV_32F);
+    double coef = 1.0/ (map.getResolution() * scaleZ * 16); // 16 for scharr pattern /2
+    if (verbose_) ROS_INFO_STREAM("Coef = "<<coef<<"                             ");
+    for (int i=0; i < mapImage.rows;i++)
     {
-        grid_map::Position position;
-        map.getPosition(*it, position);
-        double angle = std::atan(map.at("gradiant", *it)/3.0);
-        Eigen::Vector3d normal(sin(angle) * sin(map.at("orientation", *it)*2*M_PI/255.0) , sin(angle) * cos(map.at("orientation", *it)*2*M_PI/255.0) , cos(angle));
-        normal.normalize();
-        map.at("normal_x", *it) = normal.x();
-        map.at("normal_y", *it) = normal.y();
-        map.at("normal_z", *it) = normal.z();
+        for (int j=0; j < mapImage.cols;j++)
+        {
+            if(mapImage.at<uchar>(i,j) != 0)
+            {
+                double angle = std::atan(gradiant.at<float>(i,j)/coef);
+                Eigen::Vector3d normal(sin(angle) * sin(orientation.at<uchar>(i,j)*2*M_PI/255.0) , sin(angle) * cos(orientation.at<uchar>(i,j)*2*M_PI/255.0) , cos(angle));
+                normal.normalize();
+                normal_x.at<float>(i,j) = (float)normal.x();
+                normal_y.at<float>(i,j) = (float)normal.y();
+                normal_z.at<float>(i,j) = (float)normal.z();
+            }
+        }
     }
+    grid_map::GridMapCvConverter::addLayerFromImage<float, 1>(normal_x, "normal_x", map, 0.0, 1.0);
+    grid_map::GridMapCvConverter::addLayerFromImage<float, 1>(normal_y, "normal_y", map, 0.0, 1.0);
+    grid_map::GridMapCvConverter::addLayerFromImage<float, 1>(normal_z, "normal_z", map, 0.0, 1.0);
     time2 = ros::Time::now();
     duration = time2.toSec() - time1.toSec();
-    if(verbose_) ROS_INFO_STREAM(duration<<"sec");
+    if(verbose_) ROS_INFO_STREAM(duration<<"sec                                 ");
 
     /// Computation of traversability
-    if(verbose_) ROS_INFO_STREAM("Traversability of grid_map");
+    if(verbose_) ROS_INFO_STREAM("Traversability of grid_map                      ");
     time1 = ros::Time::now();
     map.add("traversability");
     for (grid_map::GridMapIterator it(map); !it.isPastEnd(); ++it) {
-        if(map.at("elevation", *it) > -0.6 && map.at("elevation", *it) < z_threshold_ && map.at("normal_z", *it) > 0.85)
+        if(map.at("elevation", *it) > -0.6 && map.at("elevation", *it) < z_threshold_ && map.at("normal_z", *it) > 0.80)
             map.at("traversability", *it) = 255.0;
         else
             map.at("traversability", *it) = 0.0;
@@ -202,18 +207,19 @@ void MeshToGridMapConverter::meshCallback(const pcl_msgs::PolygonMesh& mesh_msg)
     if(verbose_) ROS_INFO_STREAM(duration<<"sec");
 
     /// to visualize gradiant more easily in gridmap
+    cv::normalize(gradiant, gradiant,0.0, 255.0, cv::NORM_MINMAX, CV_32F);
     int amplify_magnitude = 20;
     for(unsigned int i=0;i<grad_x.rows;i++)
     {
         for(unsigned int j=0;j<grad_y.cols;j++)
         {
-            if(gradiant.at<float>(i,j)*amplify_magnitude < 255)
+            if(gradiant.at<float>(i,j)*amplify_magnitude < 255.0)
                 gradiant.at<float>(i,j) = gradiant.at<float>(i,j)*amplify_magnitude;
             else
-                gradiant.at<float>(i,j) = 255;
+                gradiant.at<float>(i,j) = 255.0;
         }
     }
-    grid_map::GridMapCvConverter::addLayerFromImage<float, 1>(gradiant, "gradiant", map, 0.0, 255.0);
+    grid_map::GridMapCvConverter::addLayerFromImage<float, 1>(gradiant, "gradiant", map, 0, 255.0);
 
     /// Publish grid map
     map.setTimestamp(mesh_msg.header.stamp.toNSec());
